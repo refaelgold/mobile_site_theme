174257d688fd29b70388f22bdffff15dc1e2a94f
nirgoldman <nir@joomi.co.il>
2014-01-22 18:10:13 +0200

    commit after one day development

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -7,10 +7,10 @@ $('<img/>').attr('src', 'images/background.jpg').load(function() {
 	jQuery('#logo').fadeIn(600, function() {
 		jQuery('h1').animate({opacity: '1', 'padding-top': '0'}, 500,function() {
 			jQuery('h2').animate({opacity: '1', 'padding-top': '0'}, 500,function() {
-				if(jQuery(window).width()<767){	
+				if(jQuery(window).width()<767){
 					jQuery('#explore').animate({opacity: '1', 'margin-top': '2em'}, 1000);
 				} else {
-					jQuery('#explore').animate({opacity: '1', 'margin-top': '4em'}, 1000);
+					jQuery('#explore').animate({opacity: '1', 'margin-top': '1em'}, 1000);
 				}
 			});
 		});
@@ -26,24 +26,24 @@ textCenter();
     });
 
     $('.text-container').css({
- 
-        left: ($(window).width() - $('.text-container').outerWidth())/2,
-        top: ($(window).height() - $('.text-container').outerHeight())/2
+
+//        left: ($(window).width() - $('.text-container').outerWidth())/2,
+//        top: ($(window).height() - $('.text-container').outerHeight())/2
 
     });
 
 }
-	
+
 jQuery(document).ready(function() {
 
 	/*-----------------------------------------------------------------------------------*/
 	/*	Navigation
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	var animate='down';
-	
+
 	jQuery(window).bind('scroll', function () {
-	
+
 		/* Animation for Top Navigation */
 		var scrollTop = jQuery(window).scrollTop();
 
@@ -74,126 +74,102 @@ jQuery(document).ready(function() {
 			}
 		});
 	});
-	
+
 	/* Responsive Menu Click */
 	jQuery('#menu-mobile').click(function(){
 		if ( jQuery("#top-navigation ul").is(":visible") ) {
 		    jQuery("#top-navigation ul").slideUp(500);
 		   	jQuery('#menu-mobile').removeClass('active');
-		} else { 
+		} else {
 		   	jQuery("#top-navigation ul").slideDown(500);
 		   	jQuery('#menu-mobile').addClass('active');
 		}
 	});
-	
+
 	/* On Resize show menu on desktop if hidden */
 	jQuery(window).resize(function() {
 		textCenter();
-	    if(jQuery(window).width()>992){	
+	    if(jQuery(window).width()>992){
 			if (jQuery("#top-navigation ul").is(":hidden") ) {
 			    jQuery("#top-navigation ul").show();
-			   	jQuery('#menu-mobile').removeClass('active');		
+			   	jQuery('#menu-mobile').removeClass('active');
 			}
 	    } else {
 	    	if (jQuery("#top-navigation ul").is(":visible") ) {
 			    jQuery("#top-navigation ul").hide();
-			   	jQuery('#menu-mobile').removeClass('active');		
+			   	jQuery('#menu-mobile').removeClass('active');
 			}
 
 
 	    }
 	});
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	Smooth Scroll - Navigation + .scroll items
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	jQuery('#top-navigation li').bind('click',function(event){
 	    var anchor = jQuery(this).find('a');
-	    
+
 	    jQuery('#top-navigation li').removeClass('active');
 	    jQuery(this).addClass('active');
-	
+
 	    jQuery('html, body').stop().animate({
 	        scrollTop: jQuery(anchor.attr('href')).offset().top-50
 	    }, 1500,'easeInOutExpo');
-	    
+
 	    /* If Mobile hide menu on select */
-	    if(jQuery(window).width()<=767){	
+	    if(jQuery(window).width()<=767){
 		    jQuery("#top-navigation ul").slideUp(500);
 			jQuery('#menu-mobile').removeClass('active');
 	    }
-	    
+
 	    event.preventDefault();
 	});
-	
+
 	jQuery('.scroll').bind('click',function(event){
 	    var anchor = jQuery(this);
-	
+
 	    jQuery('html, body').stop().animate({
 	        scrollTop: jQuery(anchor.attr('href')).offset().top-50
 	    }, 1500,'easeInOutExpo');
-	    
+
 	    /* If Mobile hide menu on select */
-	    if(jQuery(window).width()<=767){	
+	    if(jQuery(window).width()<=767){
 		    jQuery("#top-navigation ul").slideUp(500);
 			jQuery('#menu-mobile').removeClass('active');
 	    }
-	    
+
 	    event.preventDefault();
 	});
-	
-	/*-----------------------------------------------------------------------------------*/
-	/*	Studio
-	/*-----------------------------------------------------------------------------------*/
-	
-	/* Initialize Slider */
 
 
-	var swiper = jQuery('#swiper').swiper({
-		loop:true,
-		grabCursor: true,
-		autoPlay: 4000
-	});
-	
-	/* On Load swiper height should adjust to img size */
-	jQuery('.swiper-slide img').load(function() {
-		jQuery('#swiper').height(jQuery('.swiper-slide img').height());
-		jQuery('.swiper-wrapper').height(jQuery('.swiper-slide img').height());
-	});
-
-	/* On Resize swiper height should adjust to img size */
-	jQuery(window).resize(function() {
-		jQuery('#swiper').height(jQuery('.swiper-slide img').height());
-		jQuery('.swiper-wrapper').height(jQuery('.swiper-slide img').height());
-	});
-	
 	/*-----------------------------------------------------------------------------------*/
 	/*	Services
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	var curSkills="branding";
-	
+
 	/* Services Animations */
 	jQuery('.service').click(function(){
 		jQuery('.service').removeClass('active');
 		jQuery(this).addClass('active');
 		var target = jQuery(this).attr('id');
-		jQuery("#"+curSkills+"-skills").slideUp(750, 'easeInOutExpo',function(){jQuery("#"+target+"-skills").slideDown(750, 'easeInOutExpo')});
 		curSkills =jQuery(this).attr('id');
 		jQuery('html, body').stop().animate({
 		    scrollTop: jQuery('#services-list').offset().top-100
 		}, 1000,'easeInOutExpo');
-		
+
 		jQuery("#"+target+"-skills .expand").each(function() {
 			jQuery(this).css({width:0}).animate({width:jQuery(this).attr('data-width')}, 2000);
 		});
 	});
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	Works
 	/*-----------------------------------------------------------------------------------*/
-	
+
+
 	var curWork,nextWork,previousWork,offsetWork;
 
 	/* Function to load project */
@@ -217,9 +193,9 @@ jQuery(document).ready(function() {
 	    jQuery('#project').load(curWork+".html");
 		jQuery("#previous-project-name").load("work-"+previousWork+".html #project-title");
 		jQuery("#next-project-name").load("work-"+nextWork+".html #project-title");
-	
+
 	});
-	
+
 	/* Function to close project */
 	jQuery('#close-project').click(function(){
 		jQuery('iframe').attr('src', jQuery('iframe').attr('src')); // required to stop video's on exit project
@@ -227,15 +203,15 @@ jQuery(document).ready(function() {
 			jQuery("#page").fadeIn(500);
 			window.scrollTo(0,offsetWork-75);
 		});
-		
+
 	});
-	
+
 	/* Function to load next project */
 	jQuery('#next-project').click(function(){
 		var cur = nextWork;
 		jQuery("#next-project-name").animate({"right":"8em","opacity":"0"}, 500, function(){
 			jQuery("#project").fadeOut(500,function(){
-				jQuery('#project').load("work-"+cur+".html");	
+				jQuery('#project').load("work-"+cur+".html");
 				nextWork=cur+1;
 				previousWork=cur-1;
 				if (previousWork == 0){
@@ -250,13 +226,13 @@ jQuery(document).ready(function() {
 			});
 		});
 	});
-	
+
 	/* Function to load previous project */
 	jQuery('#previous-project').click(function(){
 		var cur = previousWork;
 		jQuery("#previous-project-name").animate({"left":"8em","opacity":"0"}, 500, function(){
 			jQuery("#project").fadeOut(500,function(){
-				jQuery('#project').load("work-"+cur+".html");	
+				jQuery('#project').load("work-"+cur+".html");
 				nextWork=cur+1;
 				previousWork=cur-1;
 				if (previousWork == 0){
@@ -266,17 +242,17 @@ jQuery(document).ready(function() {
 					nextWork=1;
 				}
 				jQuery("#previous-project-name").load("work-"+previousWork+".html #project-title");
-				jQuery("#next-project-name").load("work-"+nextWork+".html #project-title");	
+				jQuery("#next-project-name").load("work-"+nextWork+".html #project-title");
 				jQuery("#project").fadeIn(500);
 			});
 		});
 	});
-	
+
 	/* Works Top Bar */
 	jQuery(window).bind('scroll', function () {
-		if(jQuery(window).width()<767 && jQuery("#project-page").is(":visible")){	
+		if(jQuery(window).width()<767 && jQuery("#project-page").is(":visible")){
 			var scrollTop = jQuery(window).scrollTop();
-			
+
 			if (scrollTop > jQuery('#project-details').offset().top-125) {
 				jQuery('#project-top-bar').addClass('fixed').stop().animate({top:0},500);
 			} else if(scrollTop < jQuery('#project-details').offset().top-125){
@@ -284,17 +260,17 @@ jQuery(document).ready(function() {
 					jQuery('#project-top-bar').removeClass('fixed');
 				});
 			}
-		
+
 		}
 	});
-	
+
 	/* Filter for Works */
 	jQuery('#work-container').mixitup({
 		targetDisplayGrid: 'block' // required to fix bug in Chrome with images height
 	});
 
 	/* If we're on medium or large device animate next and previous project name while hovering arrow's */
-	if(jQuery(window).width()>767){	
+	if(jQuery(window).width()>767){
 		jQuery('#next-project').mouseenter(function() {
 			jQuery("#next-project-name").stop().animate({"right":"4em","opacity":"1"}, 500);
 		}).mouseleave(function() {
@@ -306,7 +282,7 @@ jQuery(document).ready(function() {
 			jQuery("#previous-project-name").animate({"left":"8em","opacity":"0"}, 500);
 		});
 	}
-	
+
 	/* Creates the filter menu for mobile version */
 	$('#work-filter ul').each(function(){
 		var select=$(document.createElement('select')).insertBefore($(this).parent()).addClass('visible-xs');;
@@ -314,19 +290,19 @@ jQuery(document).ready(function() {
 			option=$(document.createElement('option')).appendTo(select).val(this.href).html($(this).html()).addClass($(this).attr('data-filter'));
 		});
 	});
-	
+
 	/* Filtering for Select */
 	$('select').on('change',function(){
 		jQuery('#work-container').mixitup('filter',jQuery(this).find('option:selected').attr('class'));
 	});
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	Social
 	/*-----------------------------------------------------------------------------------*/
 
 	/* Initalize Twitter Feed */
 		$('#twitter').tweetable({
-			username: 'digitalcookers', 
+			username: 'digitalcookers',
 			time: true,
 			limit: 5,
 			replies: false,
@@ -351,7 +327,7 @@ jQuery(document).ready(function() {
 				ticker();
 			}
 		});
-	
+
 	/* Initalize Flickr Feed */
 	jQuery('#flickr').jflickrfeed({
 		limit: 12,
@@ -360,17 +336,17 @@ jQuery(document).ready(function() {
 		},
 		itemTemplate: '<div class="col-xs-3 col-sm-2 flickr-photo"><a href="{{image_b}}"target="_blank"> <img src="{{image_m}}" alt="{{title}}" /> </a></div>'
 	});
-	
+
 	/* Initalize Instagram Feed */
 	jQuery.fn.spectragram.accessData={accessToken:'2136707.12e2743.9576ae17af4e4ad4aebf6b72433c01fd',clientID:'12e27430fe054c72839c5f16f86177e1'};
-	
+
 	jQuery('#instagram').spectragram('getUserFeed',{
 	    query: 'alotofbradshaw',
 	    max: 12
 	});
 
-	
-/* Click's on Social Feed Icon */	
+
+/* Click's on Social Feed Icon */
 		jQuery('#social-feed li').click(function(){
 			jQuery('#social-feed li').removeClass('active');
 			jQuery(this).addClass('active');
@@ -385,12 +361,12 @@ jQuery(document).ready(function() {
 				jQuery('.feed').hide();
 				jQuery('#flickr').fadeIn(1000);
 			}
-		});	
+		});
 
 	/*-----------------------------------------------------------------------------------*/
 	/*	Contacts
-	/*-----------------------------------------------------------------------------------*/	
-		
+	/*-----------------------------------------------------------------------------------*/
+
 	/* Validation Form with AJAX while typing for inputs */
 	jQuery('input').bind('input propertychange', function() {
 		jQuery(this).parent().find('.error').remove();
@@ -400,38 +376,38 @@ jQuery(document).ready(function() {
 			if (jQuery(this).val() == "" || jQuery(this).val() == " ") {
 				jQuery(this).after("<span class='error'></span>");
 				jQuery(this).parent().find('.error').fadeIn('slow');
-			} else if (!checkEmail.test(jQuery(this).val())) { 
+			} else if (!checkEmail.test(jQuery(this).val())) {
 				jQuery(this).after("<span class='error'></span>");
 				jQuery(this).parent().find('.error').fadeIn('slow');
 			} else {
 				jQuery(this).after("<span class='valid'></span>");
-				jQuery(this).parent().find('.valid').fadeIn('slow');	
-			}    
+				jQuery(this).parent().find('.valid').fadeIn('slow');
+			}
 	    } else {
 			if(jQuery(this).val() == "" || jQuery(this).val() == " "){
 				jQuery(this).after("<span class='error'></span>");
-				jQuery(this).parent().find('.error').fadeIn('slow');			   
+				jQuery(this).parent().find('.error').fadeIn('slow');
 			} else {
 				jQuery(this).after("<span class='valid'></span>");
-				jQuery(this).parent().find('.valid').fadeIn('slow');	
+				jQuery(this).parent().find('.valid').fadeIn('slow');
 			}
 	    }
 	});
-	
+
 	/* Validation Form with AJAX while typing for textarea */
 	jQuery('textarea').bind('input propertychange', function() {
 		jQuery(this).parent().find('.error').remove();
-		jQuery(this).parent().find('.valid').remove();	
+		jQuery(this).parent().find('.valid').remove();
 		if(jQuery(this).val() == "" || jQuery(this).val() == " "){
 			jQuery(this).after("<span class='error'></span>");
-			jQuery(this).parent().find('.error').fadeIn('slow');			   
+			jQuery(this).parent().find('.error').fadeIn('slow');
 		} else {
 			jQuery(this).after("<span class='valid'></span>");
-			jQuery(this).parent().find('.valid').fadeIn('slow');	
+			jQuery(this).parent().find('.valid').fadeIn('slow');
 		}
-	});	
-	
-	
+	});
+
+
 	/* Validation Form with AJAX on Submit */
 	jQuery('#submit').click(function(){
 		jQuery('span.error').fadeOut('slow');
@@ -440,32 +416,32 @@ jQuery(document).ready(function() {
 		jQuery('#error').hide();
 		jQuery('#timedout').hide();
 		jQuery('#state').hide();
-		
+
 		var error = false;
-		
-		var name = jQuery('#name').val(); 
+
+		var name = jQuery('#name').val();
 		if(name == "" || name == " ") {
 			jQuery('#name').after("<span class='error'></span>");
 			jQuery('#name').parent().find('.error').fadeIn('slow');
-			error = true; 
+			error = true;
 		} else {
 			jQuery('#name').after("<span class='valid'></span>");
-			jQuery('#name').parent().find('.valid').fadeIn('slow');			
+			jQuery('#name').parent().find('.valid').fadeIn('slow');
 		}
-		
+
 		var checkEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		var email = jQuery('#email').val();
+        var email = jQuery('#email').val();
 		if (email == "" || email == " ") {
 			jQuery('#email').after("<span class='error'></span>");
 			jQuery('#email').parent().find('.error').fadeIn('slow');
 			error = true;
-		} else if (!checkEmail.test(email)) { 
+		} else if (!checkEmail.test(email)) {
 			jQuery('#email').after("<span class='error'></span>");
 			jQuery('#email').parent().find('.error').fadeIn('slow');
 			error = true;
 		} else {
 			jQuery('#email').after("<span class='valid'></span>");
-			jQuery('#email').parent().find('.valid').fadeIn('slow');			
+			jQuery('#email').parent().find('.valid').fadeIn('slow');
 		}
 
 
@@ -485,17 +461,16 @@ jQuery(document).ready(function() {
 		}
 
 
-		
-		var message = jQuery('#message').val(); 
+		var message = jQuery('#message').val();
 		if(message == "" || message == " ") {
 			jQuery('#message').after("<span class='error'></span>");
 			jQuery('#message').parent().find('.error').fadeIn('slow');
-			error = true; 
+			error = true;
 		} else {
 			jQuery('#message').after("<span class='valid'></span>");
-			jQuery('#message').parent().find('.valid').fadeIn('slow');			
+			jQuery('#message').parent().find('.valid').fadeIn('slow');
 		}
-		
+
 		if(error == true) {
 			jQuery('#error').fadeIn('slow');
 			setTimeout(function() {
@@ -503,7 +478,7 @@ jQuery(document).ready(function() {
 			}, 3000);
 			return false;
 		}
-		
+
 		var data_string = jQuery('#contact-form').serialize();
 
 
@@ -547,7 +522,7 @@ jQuery(document).ready(function() {
 				}, 3000);
 			}
 		});
-		
+
 		return false;
 	});
 });
